1518684439c6d0ecca79d03c1504b8d7763e95a9
skyblue0777 <56506708+skyblue0777@users.noreply.github.com>
2024-02-21 14:51:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import sklearn
-
 df = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
 st.line_chart(df)
@@ -12,3 +11,16 @@ st.text('hiaaaaa')
 st.text('hiaaaaa')
 st.text('hiaaaaa')
 
+import matplotlib.pyplot as plt
+
+# 데이터 생성
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# 선 그래프 그리기
+plt.plot(x, y)
+
+# 그래프에 제목과 레이블 추가
+plt.title('Simple Line Plot')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
