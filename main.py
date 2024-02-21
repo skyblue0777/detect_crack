0111291f5071ded4ef4f37543ae830e373420990
skyblue0777 <56506708+skyblue0777@users.noreply.github.com>
2024-02-21 17:05:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,11 @@ def main():
         # results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
         # st.write("This is a simple Streamlit app.")
 
-        model = torch.hub.load('./yolov5-master', 'custom',  'best.pt', source='local')  # yolov5n - yolov5x6 or custom
+        # model = torch.hub.load('./yolov5-master', 'custom',  'best.pt', source='local')  # yolov5n - yolov5x6 or custom
+        model = torch.hub.load('./yolov5-master', 'yolov5s', force_reload=True, trust_repo=True)  # or yolov5n - yolov5x6 or custom
+        im = 'https://ultralytics.com/images/zidane.jpg'  # file, Path, PIL.Image, OpenCV, nparray, list
+        results = model(im)  # inference
+        st.image(results, caption="Uploaded Image.", use_column_width=True)
         # results = model(image)  # inference
         # st.image(results, caption="Uploaded Image.", use_column_width=True)
 
