aec6a4fc97f9e3eb154436e45ac87387d7d0a1ef
skyblue0777 <56506708+skyblue0777@users.noreply.github.com>
2024-02-21 16:51:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import torch
 def main():
     st.title("Streamlit Image Upload and Display")
 
-    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+    uploaded_file = st.file_uploader("Choose an image...", type="png")
 
     if uploaded_file is not None:
         # 업로드한 파일을 이미지로 변환
